27a9d8cf200bcacd580c669e1fb114935e68fc0c
gabyiy <gabyiy2000@hotmail.com>
2021-06-09 20:42:59 +0000

    3 commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //pip3 install flask && python3 server.py
 //git add -A asa adaugam tot
 //git push
+//npm install now -g pentru a instala cite mai incolo apoi scriem now apoi dinou now si iar now  --prod ca sa facem deploy la changes
 
 console.log("lol")
