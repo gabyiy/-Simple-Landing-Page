8fa0f5589cf4372233e05624e5fcab0837eab3e4
gabyiy <gabyiy2000@hotmail.com>
2021-06-10 17:14:42 +0000

    user form

--- app.js
+++ app.js
@@ -3,4 +3,22 @@
 //git push
 //npm install now -g pentru a instala cite mai incolo apoi scriem now apoi dinou now si iar now  --prod ca sa facem deploy la changes
 
-console.log("lol")
+
+//Step 1 select the form with javascript 
+let form=document.querySelector("form");
+//step 2 atach the function send to the form submit event
+form.onsubmit=send
+//step 3 declace the fuction send and call it
+function send(e){//with e we prevent that the page wil lrefresh afer we introduce the email
+    e.preventDefault()
+alert ("ati apasat subimt")
+sendEmail()
+}
+//step 4 Get the email from the imput 
+//send the client mail to you r mail
+//thank the client
+function sendEmail(){
+    email =document.querySelector("input")
+    alert("mailu introdus este" + email.value);
+    form.innerHTML="multumim"
+}
